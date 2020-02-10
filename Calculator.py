0bc83e67fc2169aaf35470c112df8e7c638415ab
Sanketh S Huddar <40519714+sank-huddar@users.noreply.github.com>
2020-02-10 12:38:47 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+# This program adds two numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+# Add two numbers
+sum = float(num1) + float(num2)
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
